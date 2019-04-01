61bdb3bb10f144979cb1669abb735f7b490085d0
Vlad <pckltr@users.noreply.github.com>
2019-04-01 07:00:43 +0200

    Style favorites

--- spotify-search/src/FavoriteArtist.js
+++ spotify-search/src/FavoriteArtist.js
@@ -3,15 +3,27 @@ import React from 'react';
 class FavoriteArtist extends React.Component {
   render() {
     const props = this.props;
-    return (
-      <ul>
-        {props.list.map((name, key) => (
-          <li key={key}>
-            <span>{name}</span>
-            <button onClick={props.remove.bind(this, name)}>remove to favorites</button>
-          </li>
-        ))}
-      </ul>
+    return (      
+      <div className="table-container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Artist's name</th>
+              <th>Remove from favorites</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.list.map((name, key) => (
+              <tr key={key}>
+                <td className="artist-cell">{name}</td>
+                <td>
+                  <button onClick={props.remove.bind(this, name)}>remove to favorites</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
